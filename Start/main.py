af4733e66791384b9461cd19b19d76c5511696b9
GabrylchukVlad <polardeer228@gmail.com>
2026-09-07 23:02:55 +0300

    Press start

---
+++ Start/main.py
@@ -0,0 +1,19 @@
+#int
+#flat
+#Str
+#bool
+#list
+#tuple
+#dict
+#set
+age = 10
+a = 4.19
+name = "Taras"
+n = True
+list1 = [1, 2, 3, 4]
+tpl = (1, 2, 3, 4)
+dct = {10: "1", 
+    20: "20"}
+st = {1, 2, 3, 4}
+print(type(age))
+#THE END
